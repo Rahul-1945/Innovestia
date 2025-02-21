2c407be719284a6c6c16ddac36427006cc041322
Nagul71 <118661118+Nagul71@users.noreply.github.com>
2025-02-21 23:16:15 +0530

    created a route for dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <div className='grid justify-center items-center '>
-    Welcome to Thrive X
-    </div>
-    </>
-  )
-}
+import { BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import Dashboard from './components/Dashboard'
 
 export default App
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/dashboard" component={Dashboard} />
+        </Routes>
+    </Router>
+  )
+}
